e64f90a48553dc25d752ffdbc29398e24cbab1ca
mohamedelaraby <mohamedelaraby1995@gmail.com>
2020-02-18 06:59:16 -0800

    interior page templete

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -17,14 +17,30 @@ function university_files(){
     wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     
     //  Loads css stylesheet file
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('university_main_styles', get_stylesheet_uri(), Null, microtime());
 
     //  Loads javscript main file
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'),NULL,'1.0',true );
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'),NULL,microtime(),true );
 }
 
  // Hooks a function to a Specific action
+ // Hooks frontend files
  add_action( 'wp_enqueue_scripts','university_files');
 
 
+/**
+ *  Setup different theme features
+ * @return features
+ * 
+ */
+ function university_features(){
+     // Setup dynamic title for each  page
+    add_theme_support('title-tag');
+}
+
+
+//  Hooks page`s title
+add_action( 'after_setup_theme', 'university_features' );
+
 ?>
+
--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -1,19 +1,48 @@
 <?php
 
 // Header content 
-get_header(  );
+get_header();
 
+//  Display all posts
 while (have_posts()) {
     the_post(); ?>
 
-    <h1>this is a page not post </h1>
-    <h2><?php the_title(); ?> </h2>
-    <?php the_content();  ?>
+<!--  -->
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title(); ?> </h1>
+            <div class="page-banner__intro">
+                <p>DON`T FORGET TO REPLACE ME LATER</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+
+        <div class="metabox metabox--position-up metabox--with-home-link">
+            <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+        </div>
+
+        <!-- <div class="page-links">
+            <h2 class="page-links__title"><a href="#">About Us</a></h2>
+            <ul class="min-list">
+                <li class="current_page_item"><a href="#">Our History</a></li>
+                <li><a href="#">Our Goals</a></li>
+            </ul>
+        </div> -->
+
+        <!-- [ Page content ] -->
+        <div class="generic-content">
+            <?php the_content(); ?>
+        </div>
+
+    </div>
 
 
 
 <?php }
 
 // Footer content
-get_footer(  );
+get_footer();
 ?>
